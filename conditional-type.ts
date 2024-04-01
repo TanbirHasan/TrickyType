ec45199439119016127f3a9d55f1c4f77b731ec2
TanbirHasan <mdtanbir576@gmail.com>
2024-04-02 00:06:56 +0600

    update

---
+++ conditional-type.ts
@@ -0,0 +1,30 @@
+type a1 = string;
+
+type a2 = a1 extends string ? string : null;
+
+// nested conditional types
+
+type a3 = undefined;
+type a4 = number;
+
+type d = a1 extends null
+  ? null
+  : a3 extends number
+  ? number
+  : a4 extends null
+  ? null
+  : never;
+
+  type Manush = {
+    car1 : string,
+    car2 : string
+  }
+
+
+  // check korbe ai manush er car1 ase kina 
+
+  type A = keyof Manush
+
+  type CheckProperty<T,K> = K extends keyof Manush ? true : false;
+
+  type CheckOne = CheckProperty<Manush,'wife1'>
